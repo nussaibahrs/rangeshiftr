5f612fe54c900aad0d0ff7c74dfde7b8056cd4b8
Nussaibah Raja-Schoob <nussaibahraja@gmail.com>
2019-10-22 09:05:25 +0200

    initial commit

---
+++ corals_demo.R
@@ -0,0 +1,19 @@
+library(here)
+library(tidyverse)
+library(raster)
+
+# worldmap
+coast <- rworldmap::getMap()
+
+paleo.df <- read.csv("corals_Aug2019.csv") %>%
+  filter(stg == 94 & early_interval == "Late Pleistocene" &
+           between(paleolng, 130, 160) & between(paleolat, -30, -10))
+
+
+x11()
+plot(coast, xlim=c(130, 160), ylim=c(-30, -10))
+points(dat$paleolng, dat$paleolat)
+
+library(robis)
+coral.occ <- occurrence("Acropora")
+unique(coral.occ$basisOfRecord)
