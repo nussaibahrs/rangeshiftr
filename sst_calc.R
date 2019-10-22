4c4375d3db88aa081cf6fb304e2dd7262e39a7e9
Nussaibah Raja-Schoob <nussaibahraja@gmail.com>
2019-10-22 11:44:29 +0200

    file reorganisation

--- sst_calc.R
+++ sst_calc.R
@@ -9,7 +9,7 @@ HadISST.b <- crop(HadISST.b, extent(130,  160, -30, -10))
 
 ##### static file ####
 static_ann <- calc(HadISST.b, mean, na.rm=TRUE)
-writeRaster(static_ann, here("Data", "static_ann.tif"), format="GTiff",
+writeRaster(static_ann, here("data", "static_ann.tif"), format="GTiff",
             overwrite=TRUE)
 
 yr <- seq(0, 1796, 12)
@@ -21,10 +21,10 @@ for (i in 1:length(yr)){
   ann <- stack(ann,calc(HadISST.b[[(i+1):yr[i+1]]], mean))
 }
 
-writeRaster(ann, here("Data", "SST_ann.tif"), format = "GTiff", overwrite=TRUE)
+writeRaster(ann, here("data", "SST_ann.tif"), format = "GTiff", overwrite=TRUE)
 
 ##### 
-ann <- stack(here("Data", "SST_ann.tif"))
+ann <- stack(here("data", "SST_ann.tif"))
 names(ann) <- 1870:2018
 
 x11();plot(ann)
@@ -40,22 +40,22 @@ for (i in 1:149){
 x11();plot(ann)
 
 #### static SST
-static_ann <- raster(here("Data", "static_ann.tif"))
+static_ann <- raster(here("data", "static_ann.tif"))
 static_ann[static_ann > 24 & static_ann < 28] <- 1
 static_ann[static_ann !=1] <- 0
 
 plot(static_ann)
 
-writeRaster(static_ann, here("Data", "static_habitat1.tif"), format="GTiff",
+writeRaster(static_ann, here("data", "static_habitat1.tif"), format="GTiff",
             overwrite=TRUE)
 
 
 
 ####### habitat
-bath <- raster(here("Data", "bathy_5m.tif"))
+bath <- raster(here("data", "bathy_5m.tif"))
 bath <- crop(bath, extent(130,  160, -30, -10))
 
-sst_hab <- raster(here("Data", "static_habitat1.tif"))
+sst_hab <- raster(here("data", "static_habitat1.tif"))
 
 sst_hab <- resample(sst_hab, bath)
 plot(sst_hab)
@@ -69,4 +69,4 @@ plot(sst_hab)
 sst_hab[sst_hab == 2] <- 1
 plot(sst_hab)
 
-writeRaster(sst_hab, here("Data", "static_habitat2.tif"), format="GTiff", overwrite=TRUE)
+writeRaster(sst_hab, here("data", "static_habitat2.tif"), format="GTiff", overwrite=TRUE)
