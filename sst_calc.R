9854c8508b854c53989c254bea2ce82f633485c2
Nussaibah Raja-Schoob <nussaibahraja@gmail.com>
2019-10-22 11:31:55 +0200

    habitat suitability update

--- sst_calc.R
+++ sst_calc.R
@@ -48,3 +48,24 @@ plot(static_ann)
 
 writeRaster(static_ann, here("Data", "static_habitat1.tif"), format="GTiff",
             overwrite=TRUE)
+
+
+
+####### habitat
+bath <- raster(here("Data", "bathy_5m.tif"))
+bath <- crop(bath, extent(130,  160, -30, -10))
+
+sst_hab <- raster(here("Data", "static_habitat1.tif"))
+
+sst_hab <- resample(sst_hab, bath)
+plot(sst_hab)
+
+sst_hab[bath < 25 & sst_hab > 0] <- 2
+plot(sst_hab)
+
+sst_hab[sst_hab != 2] <- 0
+plot(sst_hab)
+
+sst_hab[sst_hab == 2] <- 1
+plot(sst_hab)
+
